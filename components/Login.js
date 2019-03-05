613e863e9c01a590284c4fb6d36419ece6ad5b06
Yao Yao <LukeYao233@gmail.com>
2019-03-05 13:12:26 -0500

    reset

---
+++ components/Login.js
@@ -0,0 +1,110 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TextInput, Button, Image} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+//const LoginNavigator = createStackNavigator({
+//  Login: {screen: App},
+//  CreateNewAccount: {screen: CreateNewAccount},
+//});
+
+class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require('../resources/logo.jpg')}/>
+        <Text style={styles.welcome}>Log In</Text>
+        <Text style={styles.instructions}>Username</Text>
+        <TextInput
+          style={styles.inputblock}
+          placeholder=' johndoe@mail.com'>
+        </TextInput>
+        <Text style={styles.instructions}>Password</Text>
+        <TextInput
+          style={styles.inputblock}
+          placeholder=' ••••••'>
+        </TextInput>
+        <View style={styles.button}>
+          <Button
+            title="Sign In"
+            color="#0000FF"/>
+        </View>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+        <Text style={styles.noaccount}>Don't have an account?</Text>
+        <View style={styles.button}>
+          <Button
+            title="Join Now"
+            color="#0000FF"/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class CreateNewAccount extends Component {
+  render() {
+    return (
+      <View style={styles.container}>Create New Account</View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: 290,
+    height: 140,
+  },
+  container: {
+    marginLeft: 60,
+    marginRight: 60,
+  },
+  button: {
+    marginTop: 5,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold',
+    color: '#808080',
+  },
+  instructions: {
+    color: '#808080',
+    marginBottom: 5,
+    fontSize: 15,
+  },
+  inputblock: {
+    borderWidth: 1,
+    marginBottom: 10,
+    fontSize: 20,
+  },
+  forgot: {
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    marginTop: 10,
+  },
+  noaccount: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 50,
+    fontWeight: 'bold',
+    color: '#808080',
+  }
+});
+
+//const App = createAppContainer(LoginNavigator);
+
+
+export default Login;
